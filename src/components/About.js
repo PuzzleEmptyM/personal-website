861c20a630b5760f0869ad427b24cd7058103efe
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-06-11 18:37:28 -0500

    updated 'about me' and fixed some formatting

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,14 @@ const About = () => {
         <img src={`${process.env.PUBLIC_URL}/images/puzzle_profile.jpg`} alt="Profile" className="profile-picture" />
       </div>
       <h2>About Me</h2>
-      <p>Hi! I'm Puzzle, a full-stack engineering student with a passion for creating dynamic and user-friendly web applications. I love solving complex problems and bringing ideas to life through code.</p>
-      <p>In my spare time, I enjoy playing puzzle games, drawing, and exploring new technologies.</p>
+      <br></br>
+      <p>As a Full Stack Developer student at Atlas School, I’ve embraced a curriculum that emphasizes practical experience over traditional lectures. The school's hands-on, project-based approach has allowed me to gain in-depth knowledge of both front-end and back-end technologies, including JavaScript, HTML, Python, and C.</p>
+      <br></br>
+      <p>Outside of my studies, I’ve taken on projects that provide invaluable real-world experience. For example, I am developing a website for Samuel Claus and am currently optimizing AI-generated code for Scale AI. These experiences have honed my ability to deliver efficient and user-friendly solutions. Additionally, my background in customer service and project management from Genesis Health Clubs has equipped me with strong communication skills and a unique perspective on user experience and business strategy.</p>
+      <br></br>
+      <p>I’m eager to bring my skills and enthusiasm to a dynamic tech team and contribute to impactful projects. As I approach graduation, I look forward to joining an innovative organization where I can continue to grow and make a tangible difference through my work.</p>
+      <br></br>
+      <p>Let's connect and collaborate in the ever-evolving world of technology!</p>
       <div className="shape circle blue" style={{ top: '10%', left: '5%' }}></div>
       <div className="shape triangle pink" style={{ bottom: '15%', right: '5%' }}></div>
       <div className="shape circle purple" style={{ top: '40%', left: '70%' }}></div>
