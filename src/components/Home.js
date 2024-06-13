e9784dab54b029dbde191df8783a3dee2f681a97
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-06-12 19:26:44 -0500

    it is good enough

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { scrollToSection } from '../utils/smoothScroll';
 
 const Home = () => (
   <section id="home">
@@ -6,13 +7,13 @@ const Home = () => (
       <img src={`${process.env.PUBLIC_URL}/images/puzzle_profile.jpg`} alt="Profile" className="profile-picture" />
     </div>
     <h1 className="home-heading">Hi, I'm Puzzle,</h1>
-    <h2 className="home-subheading">Full-Stack Engineering Student based in the USA.</h2>
+    <h2 className="home-subheading">Full-Stack Engineering Student based in Tulsa, OK.</h2>
     <p className="home-description">
       Dynamic and highly motivated Software Engineer with extensive experience. Proficient in both front-end and back-end development.
     </p>
     <div className="home-buttons">
-      <button>Contact me</button>
-      <button>My Resume</button>
+      <button onClick={() => scrollToSection('contact')}>Contact me</button>
+      <button onClick={() => scrollToSection('resume')}>My Resume</button>
     </div>
   </section>
 );
