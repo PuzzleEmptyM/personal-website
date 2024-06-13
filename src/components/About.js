00058f74a43abb3f695312de5e07ce4d0365cc06
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-06-12 19:29:22 -0500

    it is good enough

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,6 @@ const About = () => {
       <p>My experience in customer service and project management at Genesis Health Clubs has sharpened my communication skills and given me a keen insight into user experience and business strategy. As I near graduation, I am eager to join a dynamic tech team where I can contribute to meaningful projects and continue my professional growth.</p>
       <br></br>
       <p>Let's connect and explore opportunities in the vibrant tech landscape!</p>
-      <div className="shape circle blue" style={{ top: '10%', left: '5%' }}></div>
-      <div className="shape triangle pink" style={{ bottom: '15%', right: '5%' }}></div>
-      <div className="shape circle purple" style={{ top: '40%', left: '70%' }}></div>
-      <div className="shape triangle blue" style={{ bottom: '45%', right: '70%' }}></div>
-      <div className="shape circle pink" style={{ top: '25%', left: '80%' }}></div>
-      <div className="shape triangle purple" style={{ bottom: '25%', right: '80%' }}></div>
     </section>
   );
 };
