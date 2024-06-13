e0d7f015438f30fdf0cc2bf21c224239800c3604
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-06-13 14:59:27 -0500

    Updates

---
+++ precache-manifest.807fd377e7141cadc8698383ec6a7422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3d1c9661a408fefc8c74323c52cc50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3a8cd322fc86ef2c09",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "b3ec1a8a0e2571e574db",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d3a8cd322fc86ef2c09",
+    "url": "/static/js/2.f637bc6c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.f637bc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50467c038bc69b7564b6",
+    "url": "/static/js/3.dc75e812.chunk.js"
+  },
+  {
+    "revision": "b3ec1a8a0e2571e574db",
+    "url": "/static/js/main.35bc4532.chunk.js"
+  },
+  {
+    "revision": "a23346fa40f2e588e1b4",
+    "url": "/static/js/runtime-main.a9d01d2c.js"
+  }
+]);
