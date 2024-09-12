0db0f5dc1d3a5b2cb1dc36a93f07081adc493957
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-09-12 12:13:02 -0500

    updates to resume

--- code/src/components/Resume.js
+++ code/src/components/Resume.js
@@ -14,18 +14,24 @@ const Resume = () => {
       <p><strong>Marketing Manager</strong></p>
       <p>Samuel Claus, Jan 2021 - Present</p>
       <br></br>
-      <p>Manager, Marketer and Designer of samuelclaus.com. Job consists in contacting potential customers, reaching out to local art events, constantly updating the website, accompanying Samuel at every art event to help manage sales, and strategizing for constant improvement of our services.</p>
+      <p>Manager, Marketer and Designer of samuelclaus.com. Job consists in: <br></br>
+        - Contacting potential customers <br></br>
+        - Reaching out to local art events <br></br>
+        - Constantly updating the website <br></br>
+        - Upkeep for Yelp and Google business pages <br></br>
+        - Accompanying Samuel at every art event to help manage sales <br></br>
+        - Strategizing for constant improvement of our services</p>
       <br></br>
       <p><strong>Coder - AI Training</strong></p>
       <p>Scale AI, Jan 2024 - Present</p>
       <br></br>
       <p>
-        - Evaluating the quality of AI-generated code.<br></br>
-        - Writing functional and efficient code.<br></br>
-        - Optimizing code to run at maximum efficiency.<br></br>
-        - Writing robust test cases to confirm code works efficiently.<br></br>
-        - Writing human-readable summaries of coding problems.<br></br>
-        - Writing explanations of how code can solve problems.</p>
+        - Evaluating the quality of AI-generated code<br></br>
+        - Writing functional and efficient code<br></br>
+        - Optimizing code to run at maximum efficiency<br></br>
+        - Writing robust test cases to confirm code works efficiently<br></br>
+        - Writing human-readable summaries of coding problems<br></br>
+        - Writing explanations of how code can solve problems</p>
     </section>
   );
 };
