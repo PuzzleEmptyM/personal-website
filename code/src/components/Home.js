e71bb562108c14baf291f044cb33301964118630
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-12-09 15:38:13 -0600

    minor tweaks

--- code/src/components/Home.js
+++ code/src/components/Home.js
@@ -6,8 +6,8 @@ const Home = () => (
     <div className="profile-container">
       <img src={`${process.env.PUBLIC_URL}/images/puzzle_profile.jpg`} alt="Profile" className="profile-picture" />
     </div>
-    <h1 className="home-heading">Hi, I'm Puzzle,</h1>
-    <h2 className="home-subheading">Full-Stack Engineering Student based in Tulsa, OK.</h2>
+    <h1 className="home-heading">Hi, I'm Puzzle.</h1>
+    <h2 className="home-subheading">Full-Stack Engineering Student <br/> based in Tulsa, OK.</h2>
     <p className="home-description">
       Dynamic and highly motivated Software Engineer with extensive experience. Proficient in both front-end and back-end development.
     </p>
