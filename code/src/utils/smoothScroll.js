379714e177aa4049e2c059d110f34fb7c833240b
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-12-09 15:42:02 -0600

    adjusted auto scroll

--- code/src/utils/smoothScroll.js
+++ code/src/utils/smoothScroll.js
@@ -1,6 +1,13 @@
 export const scrollToSection = (id) => {
   const section = document.getElementById(id);
   if (section) {
-    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    const headerOffset = 35; // Adjust this value to match your header height
+    const elementPosition = section.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
   }
 };
