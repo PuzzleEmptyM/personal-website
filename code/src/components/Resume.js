bd21a23553e2132c17d3562e22850e8088bb8fee
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-12-20 08:50:33 -0600

    updated resume link

--- code/src/components/Resume.js
+++ code/src/components/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
   return (
     <section id="resume" ref={ref} className={isVisible ? 'section-visible' : ''}>
       <h2>Resume</h2>
-      <p>Download my <a href="https://drive.google.com/file/d/1vZATnAUZw0pJxdJZ3e1aSkgqTfHdHIRN/view?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a>.</p>
+      <p>Download my <a href="https://drive.google.com/file/d/1qaZLtY4LiR_A5jDQELbc1tLI7NDDdNXk/view?usp=drive_link" target="_blank" rel="noopener noreferrer">resume</a>.</p>
       <br></br>
       <br></br>
 
